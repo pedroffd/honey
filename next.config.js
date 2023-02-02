5f5e3ee6cad6ed630b0088accb7f76b20d99d22a
pedroffd <pedro.alcarin@gmail.com>
2023-02-02 14:02:50 -0400

    basic structure adjustments

--- next.config.js
+++ next.config.js
@@ -1,16 +1,44 @@
-const withPWA = require("next-pwa");
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
   images: {
-    domains: ["i.ibb.co", "ibb.co"],
+    domains: [
+      'cdn.worldvectorlogo.com',
+      'lh3.googleusercontent.com',
+      '1.bp.blogspot.com',
+      'image.api.playstation.com',
+      'cdn.cloudflare.steamstatic.com',
+      'i.ytimg.com',
+      'verdict.co.uk',
+    ],
   },
-  env: {
-    publishableKey: `${process.env.STRIPE_PUBLIC_KEY}`,
+  webpack5: true,
+  webpack: (config) => {
+    // eslint-disable-next-line no-param-reassign
+    config.resolve.fallback = { fs: false, process: false }
+    return config
   },
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
   },
-});
+}
+
+module.exports = nextConfig
--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,7 @@
 module.exports = {
-    plugins: [
-      'tailwindcss/nesting',
-      'tailwindcss',
-      'autoprefixer',
-      'postcss-flexbugs-fixes',
-      [
-        'postcss-preset-env',
-        {
-          autoprefixer: {
-            flexbox: 'no-2009',
-          },
-          stage: 3,
-          features: {
-            'custom-properties': false,
-          },
-        },
-      ],
-    ],
-  }
-  
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
+}
