6107f86762b24d841c82df70cfcecb3a002fd862
pedroffd <pedro.alcarin@gmail.com>
2023-02-21 21:39:24 -0300

    header component ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "157155d6c981564d"; }
+/******/ 		__webpack_require__.h = function() { return "f30e5ce87fe2b1d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
