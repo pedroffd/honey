38775685c5ae212ec9c5bc2fe0f6802cc66dd03d
pedroffd <pedro.alcarin@gmail.com>
2023-03-09 15:21:45 -0300

    footer max width alignment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d00a357ed7c741b6"; }
+/******/ 		__webpack_require__.h = function() { return "fe36b9d1a34f8e53"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => (
           </span>
         </a>
       </div>
-      <div className="md:flex md:justify-between max-w-screen-lg mx-auto px-6">
+      <div className="md:flex md:justify-between max-w-screen-lg">
         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-4">
           <div className=" divide-y divide-red-400 mr-11 min-w-full ">
             <h3 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
@@ -120,20 +120,20 @@ const Footer = () => (
               </li>
               <li>
                 <form action="">
-                  <div className="gird-cols-1 grid items-center justify-center gap-4 md:grid-cols-3">
+                  <div className="grid-cols-2 grid md:grid-cols-2">
                     <div
                       className="relative md:mb-6"
                       data-te-input-wrapper-init
                     >
                       <input
                         type="text"
-                        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-secondary-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                        className="peer block min-h-[auto] rounded border-0 bg-transparent py-[0.32rem] px-3 outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-secondary-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                         id="exampleFormControlInput1"
                         placeholder="Email address"
                       />
                       <label
                         htmlFor="exampleFormControlInput1"
-                        className="  bg-white pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-secondary-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-secondary-200 dark:peer-focus:text-secondary-200"
+                        className="w-44 pl-2.5 pt-2 pb-2 rounded-full bg-white pointer-events-none absolute top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-secondary-200 dark:peer-focus:text-secondary-200"
                       >
                         Email address
                       </label>
@@ -142,7 +142,7 @@ const Footer = () => (
                     <div className="mb-6 md:mr-auto">
                       <button
                         type="button"
-                        className="bg-mainPink-100 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                        className="bg-mainPink-100 inline-block rounded-full ml-3 bg-primary pl-2 pr-2 pt-2.5 pb-2 h-10 w-24 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                         data-te-ripple-init
                         data-te-ripple-color="light"
                       >
