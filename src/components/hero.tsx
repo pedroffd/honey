b02762f7ee6e2a12b4af085eaf2956d12cc9e141
Pedro Souza <pedro.alcarin@gmail.com>
2023-03-14 19:21:53 -0300

    Hero Section (#6)
    * hero section initial skeleton
    
    * cards divided and background style included
    
    * skeleton for hero section finished

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,8 @@
-const Footer = () => (
+const Hero = () => (
   <div className="w-full">
-    <div className="max-w-screen-xl p-5 mx-auto dark:bg-gray-800 dark:text-gray-100">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-0 lg:grid-rows-2">
-        <div
-          className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 md:col-span-2 lg:row-span-2 lg:h-full group dark:bg-gray-500"
-          style="background-image: url(https://source.unsplash.com/random/245x320);"
-        >
+    <div className="max-w-screen-xl pl-8 pr-8 pt-4 pb-4 mx-auto">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:gap- lg:grid-rows-2 h-96">
+        <div className="bg-heroMain relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer md:col-span-2 lg:row-span-2  group dark:bg-gray-500">
           <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
           <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
             <a
@@ -32,11 +29,35 @@ const Footer = () => (
             </a>
           </h2>
         </div>
-        <div
-          className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500"
-          style='background-image: url("https://source.unsplash.com/random/240x320");'
-        >
+        <div className="bg-heroSecond relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer group dark:bg-gray-500">
           <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
+          <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="px-3 py-2 text-xs font-semibold tracking-wider uppercase hover:underline dark:text-gray-100 dark:bg-violet-400"
+            >
+              Sports
+            </a>
+            <div className="flex flex-col justify-start text-center dark:text-gray-100">
+              <span className="text-3xl font-semibold leading-none tracking-wide">
+                19
+              </span>
+              <span className="leading-none uppercase">Jul</span>
+            </div>
+          </div>
+          <h2 className="z-10 p-5">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="font-medium text-md group-hover:underline dark:text-gray-100"
+            >
+              Doloribus sit illo necessitatibus architecto exercitationem enim
+            </a>
+          </h2>
+        </div>{' '}
+        <div className="bg-lightPink relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer group dark:bg-gray-500">
+          <div className="absolute top-0 bottom-0 left-0 right-0 bg-lightPink dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
           <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
             <a
               rel="noopener noreferrer"
@@ -63,99 +84,9 @@ const Footer = () => (
             </a>
           </h2>
         </div>
-        <div
-          className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500"
-          style='background-image: url("https://source.unsplash.com/random/241x320");'
-        >
-          <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
-          <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-3 py-2 text-xs font-semibold tracking-wider uppercase hover:underline dark:text-gray-100 dark:bg-violet-400"
-            >
-              Health
-            </a>
-            <div className="flex flex-col justify-start text-center dark:text-gray-100">
-              <span className="text-3xl font-semibold leading-none tracking-wide">
-                01
-              </span>
-              <span className="leading-none uppercase">Aug</span>
-            </div>
-          </div>
-          <h2 className="z-10 p-5">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="font-medium text-md group-hover:underline dark:text-gray-100"
-            >
-              Inventore reiciendis aliquam excepturi
-            </a>
-          </h2>
-        </div>
-        <div
-          className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500"
-          style='background-image: url("https://source.unsplash.com/random/242x320");'
-        >
-          <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
-          <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-3 py-2 text-xs font-semibold tracking-wider uppercase hover:underline dark:text-gray-100 dark:bg-violet-400"
-            >
-              Science
-            </a>
-            <div className="flex flex-col justify-start text-center dark:text-gray-100">
-              <span className="text-3xl font-semibold leading-none tracking-wide">
-                28
-              </span>
-              <span className="leading-none uppercase">Jul</span>
-            </div>
-          </div>
-          <h2 className="z-10 p-5">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="font-medium text-md group-hover:underline dark:text-gray-100"
-            >
-              Officiis mollitia dignissimos commodi optio vero animi
-            </a>
-          </h2>
-        </div>
-        <div
-          className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 group dark:bg-gray-500"
-          style='background-image: url("https://source.unsplash.com/random/243x320");'
-        >
-          <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
-          <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-3 py-2 text-xs font-semibold tracking-wider uppercase hover:underline dark:text-gray-100 dark:bg-violet-400"
-            >
-              Sports
-            </a>
-            <div className="flex flex-col justify-start text-center dark:text-gray-100">
-              <span className="text-3xl font-semibold leading-none tracking-wide">
-                19
-              </span>
-              <span className="leading-none uppercase">Jul</span>
-            </div>
-          </div>
-          <h2 className="z-10 p-5">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="font-medium text-md group-hover:underline dark:text-gray-100"
-            >
-              Doloribus sit illo necessitatibus architecto exercitationem enim
-            </a>
-          </h2>
-        </div>
       </div>
     </div>
   </div>
 )
 
-export { Footer }
+export { Hero }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,17 @@
 import Head from 'next/head'
 import Header from '../components/header'
 import { Footer } from '../components/footer'
-
+import { Hero } from '../components/hero'
 export default function Home() {
   return (
     <>
+      {' '}
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
-      <div className="min-h-screen pt-0 pr-2 flex flex-col justify-center items-center">
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <div className="h-16 pr-8 pl-9">
-          {' '}
-          dasfds
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div>
-      </div>
+      <Hero />
       <Footer />
     </>
   )
