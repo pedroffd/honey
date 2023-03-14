9a43f43094d322a9db2dbadfd310f473476bc477
pedroffd <pedro.alcarin@gmail.com>
2023-03-14 19:20:59 -0300

    skeleton for hero section finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,13 @@ import { Hero } from '../components/hero'
 export default function Home() {
   return (
     <>
+      {' '}
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
-      <div className="min-h-screen pt-0 pr-2 flex flex-col justify-center items-center">
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Hero />
-        {/*   <div className="h-16 pr-8 pl-9">
-          {' '}
-          dasfds
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div> */}
-      </div>
+      <Hero />
       <Footer />
     </>
   )
