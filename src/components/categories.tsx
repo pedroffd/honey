2faeaa8a6fe6e379d827807826d281749bd430a6
pedroffd <pedro.alcarin@gmail.com>
2023-04-04 18:59:35 -0300

    text placement adjusts

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -3,22 +3,26 @@ import CategoryIcon from './category_icon'
 const Categories = () => {
   return (
     <section className="md:flex-row max-w-screen-xl pl-8 pr-8 pt-4 pb-4 mx-auto">
-      <div className="py-4 text-black">
+      <div className="py-4 text-black ">
         <div className="mx-auto flex flex-col md:flex-row text-center">
-          <div className="flex flex-col w-full">
-            <h3 className="text-xl md:text-xl my-4 leading-relaxed md:leading-snug">
+          <div className="flex flex-col w-3/6 divide-y divide-red-400 mr-8 ml-80">
+            <h3 className="mt-8"></h3>
+            <p className="text-sm "></p>
+          </div>
+          <div className="flex flex-col w-2/6 items-center">
+            <h3 className="text-xl md:text-xl my-4 leading-relaxed md:leading-snug fixed top-60 pt-2">
               Categories
             </h3>
-            <div className="font-sans text-sm md:text-lg mb-2">
-              <p>Love. Shop. Discover.</p>
-            </div>
+          </div>
+          <div className="flex flex-col w-3/6 divide-y divide-red-400 ml-8 mr-80">
+            <h3 className="mt-8"></h3>
+            <p className="text-sm "></p>
           </div>
         </div>
-
-        <div
-          className="flex flex-wrap text-center md:text-center md:px-48 lg:px-8
-        justify-center"
-        >
+        <p className="font-sans text-sm md:text-lg mb-2 justify-center text-center mt-4">
+          Love. Shop. Discover.
+        </p>
+        <div className="flex flex-wrap text-center md:text-center md:px-48 lg:px-8 justify-center">
           <CategoryIcon color="#fc5d9d" width="50" height="50" />
           <CategoryIcon color="#fc5d9d" width="50" height="50" />
           <CategoryIcon color="#fc5d9d" width="50" height="50" />
