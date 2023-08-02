deb9ce221fdd68a69e491f1c92a9a9076957056f
pedroffd <pedro.alcarin@gmail.com>
2023-08-02 20:40:40 -0300

    hero section finished for web and mobile view

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -68,13 +68,64 @@ const HeroMobile: React.FC<HeroProps> = ({ highlightProducts }) => {
     )
 }
 
+const HeroWeb: React.FC<HeroProps> = ({ highlightProducts }) => {
+    return (
+        <div className="w-full">
+            <div className="max-w-screen-xl pl-8 pr-8 pt-4 pb-4 mx-auto">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:gap- lg:grid-rows-2 h-96">
+                    {highlightProducts.map((product, index) => (
+                        <div
+                            key={index}
+                            className={`bg-${
+                                product.bg
+                            } relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer dark:bg-gray-500 group ${
+                                index === 0 ? 'md:col-span-2 lg:row-span-2' : ''
+                            }`}
+                        >
+                            <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:via-transparent dark:from-gray-900 dark:to-gray-900"></div>
+                            <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+                                <a
+                                    rel="noopener noreferrer"
+                                    href="#"
+                                    className="px-3 py-2 text-xs font-semibold tracking-wider uppercase hover:underline dark:text-gray-100 dark:bg-violet-400"
+                                >
+                                    {product.topic}
+                                </a>
+                                <div className="flex flex-col justify-start text-center dark:text-gray-100">
+                                    <span className="text-3xl font-semibold leading-none tracking-wide">
+                                        {product.date.split(' ')[0]}
+                                    </span>
+                                    <span className="leading-none uppercase">
+                                        {product.date.split(' ')[1]}
+                                    </span>
+                                </div>
+                            </div>
+                            <h2 className="z-10 p-5">
+                                <a
+                                    rel="noopener noreferrer"
+                                    href={product.href}
+                                    className="font-medium text-md group-hover:underline lg:text-2xl lg:font-semibold dark:text-gray-100"
+                                >
+                                    {product.title}
+                                </a>
+                            </h2>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { HeroWeb }
+
 const Hero = () => {
     const isMobile = useIsMobile()
 
     return isMobile ? (
         <HeroMobile highlightProducts={highlightProducts} />
     ) : (
-        <div>empty</div>
+        <HeroWeb highlightProducts={highlightProducts} />
     )
 }
 
